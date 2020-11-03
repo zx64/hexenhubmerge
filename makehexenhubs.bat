9115a8765b73b19edc41f68ff8d63d24cddf2d1b
zx64 <zx64@users.noreply.github.com>
2020-11-03 21:45:55 +0000

    Bail out early of batch conversion if something fails.

--- makehexenhubs.bat
+++ makehexenhubs.bat
@@ -5,13 +5,17 @@ del hexhubs.pk3 2>nul
 mkdir tmp\maps 2>nul
 pushd tmp
 pushd maps
-%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map01.wad -n MAP01 MAP01
-%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map02.wad -n MAP02 MAP02 MAP03 MAP04 MAP05 MAP06
-%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map03.wad -n MAP03 MAP13 MAP08 MAP09 MAP10 MAP11 MAP12
-%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map04.wad -n MAP04 MAP27 MAP32 MAP33 MAP34 MAP28 MAP30 MAP31
-%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map05.wad -n MAP05 MAP22 MAP21 MAP23 MAP24 MAP25 MAP26
-%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map06.wad -n MAP06 MAP35 MAP36 MAP38 MAP37 MAP39
-%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map07.wad -n MAP07 MAP40
+%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map01.wad -n MAP01 MAP01 || goto error
+%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map02.wad -n MAP02 MAP02 MAP03 MAP04 MAP05 MAP06 || goto error
+%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map03.wad -n MAP03 MAP13 MAP08 MAP09 MAP10 MAP11 MAP12 || goto error
+%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map04.wad -n MAP04 MAP27 MAP32 MAP33 MAP34 MAP28 MAP30 MAP31 || goto error
+%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map05.wad -n MAP05 MAP22 MAP21 MAP23 MAP24 MAP25 MAP26 || goto error
+%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map06.wad -n MAP06 MAP35 MAP36 MAP38 MAP37 MAP39 || goto error
+%~dp0\hexenhubmerge.py %~dp0\HEXEN.WAD map07.wad -n MAP07 MAP40 || goto error
 popd
-7z.exe a -r -tzip ..\hexenhubs.pk3 *
+7z.exe a -r -tzip ..\hexenhubs.pk3 * || goto error
 popd
+exit /b 0
+
+:error
+exit /b 1
