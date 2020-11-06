ce8c7a5e35da46dc746f8ef57dc40b9298831d45
zx64 <zx64@users.noreply.github.com>
2020-11-06 20:08:03 +0000

    See what it looks like when you remove all entries with a direct back link.

--- viz.py
+++ viz.py
@@ -1,6 +1,7 @@
 from graphviz import Digraph
 import pickle
 
+
 def viz(name, exits, entries):
     for source in ("exits", "entries"):
         dot = Digraph(comment=f"{name}-{source}")
@@ -11,18 +12,21 @@ def viz(name, exits, entries):
             for edge in nodeedges:
                 edges.add((node, edge))
 
-        for node in sorted(g.keys()):
-            dot.node(node)
         for edge in edges:
+            a, b = edge
+            if (b, a) in edges:
+                continue
             dot.edge(*edge)
 
         dot.render(f"{name}-{source}.gv", format="png")
+
 
 def main():
     for name in ("hexen", "hexdd"):
         with open(f"{name}.wad.pickle", "rb") as f:
             print(name)
             viz(name, *pickle.load(f))
+
 
 if __name__ == "__main__":
     main()
