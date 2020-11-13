594db13337a0e86909514fa984780a639d505f24
zx64 <zx64@users.noreply.github.com>
2020-11-13 08:39:13 +0000

    Use new opcode iterator to find exits.

--- findhubs.py
+++ findhubs.py
@@ -1,54 +1,22 @@
 import click
 import omg
 import pickle
-import re
-import struct
 from listacs import acsutil
 from collections import defaultdict
 
 
-def OP(name):
-    return getattr(acsutil, f"PCD_{name}")
+PUSHNUMBER = acsutil.PCD_PUSHNUMBER
+IFNOTGOTO = acsutil.PCD_IFNOTGOTO
 
 
-# Known prefix for a deathmatch only (Gametype() == 2) script
-ifdm = struct.pack(
-    "<5I", OP("GAMETYPE"), OP("PUSHNUMBER"), 2, OP("EQ"), OP("IFNOTGOTO")
-)
+def OP(name, *args):
+    return (acsutil.pcode_index[name], *args)
 
-pushnumber = re.compile(".*: PUSHNUMBER (.*)$")
-lspec2 = re.compile(".*: LSPEC2 74$")
-lspec2direct = re.compile(".*: LSPEC2DIRECT 74, ([^,]*), (.*)$")
-setlinespecial = re.compile(".*: SETLINESPECIAL$")
 
-"""
-Example dissassembled bytecode sequences to search for:
-(HEXDD MAP33)
-         396: PUSHNUMBER 34 <- arg0
-         404: PUSHNUMBER 0 <- arg1
-         412: LSPEC2 74
-
-(HEXDD MAP42)
-        2404: LSPEC2DIRECT 74, 41, 0
-
-(HEXEN MAP02)
-         928: PUSHNUMBER 74
-         936: PUSHNUMBER 5 <- arg0
-         944: PUSHNUMBER 0 <- arg1
-         952: PUSHNUMBER 0
-         960: PUSHNUMBER 0
-         968: PUSHNUMBER 0
-         976: SETLINESPECIAL
-
-However, some exits are conditional on being deathmatch (or not):
-(HEXDD MAP60)
-        2336: GAMETYPE
-        2340: PUSHNUMBER 2
-        2348: EQ
-        2352: IFNOTGOTO 2376
-        2360: LSPEC2DIRECT 74, 41, 0
-        2376: TERMINATE
-"""
+lspec2 = OP("LSPEC2", 74)
+lspec2direct = OP("LSPEC2DIRECT", 74)
+setlinespecial = OP("SETLINESPECIAL")
+gametype_eq_2 = [OP("GAMETYPE"), OP("PUSHNUMBER", 2), OP("EQ")]
 
 
 def mapname(mapnum):
@@ -62,7 +30,7 @@ def find_exits(linedefs, behavior):
     ACTION_NEWLEVEL = 74
     # Grab arg0 from all linedefs with action == 74
     exits = set(
-        mapname(linedefs[i + LD_OFFSET_ARG0])
+        linedefs[i + LD_OFFSET_ARG0]
         for i in range(0, len(linedefs), s)
         if linedefs[i + LD_OFFSET_ACTION] == ACTION_NEWLEVEL
     )
@@ -70,42 +38,52 @@ def find_exits(linedefs, behavior):
     acs = acsutil.Behavior(behavior)
 
     for script in acs.scripts:
-        bytecode = acs.data[script.ptr : script.end]
-
-        # Check for if (Gametype() == 2) prefix
-        if bytecode.startswith(ifdm):
+        opcodes = list(script.opcodes())
+        if not opcodes:
             continue
 
-        # TODO: Search for actual opcodes instead of string matching!
-        listing = list(script.disassemble())
-        for idx, opcode in enumerate(listing):
-            if lspec2.match(opcode):
-                # TODO: Assumes params are pushed in immediate prior opcodes
-                # Stack could have been prepared in earlier instructions
-                arg0 = pushnumber.match(listing[idx - 2])
-                assert arg0 is not None
-                exits.add(mapname(int(arg0.group(1))))
-            elif m := lspec2direct.match(opcode):
-                exits.add(mapname(int(m.group(1))))
-            elif setlinespecial.match(opcode):
-                # TODO: Assumes params are pushed in immediate prior opcodes
-                # Stack could have been prepared in earlier instructions
-                special = pushnumber.match(listing[idx - 6])
-                if special is None:
-                    continue
-                try:
-                    if int(special.group(1)) != ACTION_NEWLEVEL:
-                        continue
-                except ValueError:
-                    continue
-                arg0 = pushnumber.match(listing[idx - 5])
-                assert arg0 is not None
-                exits.add(mapname(int(arg0.group(1))))
-            else:
-                # TODO: Any other opcodes to look for?
-                pass
+        # Check for if (Gametype() == 2) prefix
+        if opcodes[0:3] == gametype_eq_2 and opcodes[3][0] == IFNOTGOTO:
+            # HEXDD MAP60:
+            # GAMETYPE
+            # PUSHNUMBER 2
+            # EQ
+            # IFNOTGOTO 2376
+            # LSPEC2DIRECT 74, 41, 0
+            # TERMINATE
+            continue
 
-    return sorted(exits)
+        for idx, opcode in enumerate(opcodes):
+            if opcode == lspec2:
+                # HEXDD MAP33:
+                # idx - 2: PUSHNUMBER 34 <- arg0
+                # idx - 1: PUSHNUMBER 0 <- arg1
+                # idx    : LSPEC2 74
+                op, arg0 = opcodes[idx - 2]
+                assert op == PUSHNUMBER
+                exits.add(arg0)
+            elif opcode[0:2] == lspec2direct:
+                # HEXDD MAP42: LSPEC2DIRECT 74, 41, 0
+                exits.add(opcode[2])
+            elif opcode == setlinespecial:
+                # HEXEN MAP02
+                # idx - 6: PUSHNUMBER 74
+                # idx - 5: PUSHNUMBER 5 <- arg0
+                # idx - 4: PUSHNUMBER 0 <- arg1
+                # idx - 3: PUSHNUMBER 0
+                # idx - 2: PUSHNUMBER 0
+                # idx - 1: PUSHNUMBER 0
+                # idx    : SETLINESPECIAL
+                op, special = opcodes[idx - 6]
+                if op != PUSHNUMBER:
+                    continue
+                if special != ACTION_NEWLEVEL:
+                    continue
+                op, arg0 = opcodes[idx - 5]
+                assert op == PUSHNUMBER
+                exits.add(arg0)
+
+    return sorted(mapname(e) for e in exits)
 
 
 @click.command()
